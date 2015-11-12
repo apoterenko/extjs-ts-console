7349ab002576705c063e24815ceb638a157a4436
apoterenko <a.poterenko@gmail.com>
2015-11-12 19:39:46 +0300

    *FieldTrigger: GroupField plugin supported

--- app/plugin/form/field/FieldTrigger.js
+++ app/plugin/form/field/FieldTrigger.js
@@ -4,212 +4,242 @@
  * Provides an additional functionality for Fields that adds a clickable trigger elements (looks like a combobox by default) on the right side.
  */
 Ext.define('ManagementConsole.plugin.form.field.FieldTrigger', {
-    extend: 'Ext.AbstractPlugin',
+	extend: 'Ext.AbstractPlugin',
 
-    /**
-     * @inheritdoc
-     */
-    alias: 'plugin.field.trigger',
+	/**
+	 * @inheritdoc
+	 */
+	alias: 'plugin.field.trigger',
 
-    /**
-     * @private
-     */
-    triggerUI: 'fieldtrigger',
+	/**
+	 * @private
+	 */
+	triggerUI: 'fieldtrigger',
 
-    /**
-     * @protected
-     */
-    fieldTriggerPrefixCls: 'field-trigger-',
+	/**
+	 * @protected
+	 */
+	fieldTriggerPrefixCls: 'field-trigger-',
 
-    /**
-     * Array actions
-     */
+	/**
+	 * Array actions
+	 */
     actions: [],
 
-    /**
-     * @private
-     */
-    inject: [
-        'eventManager'
-    ],
+	/**
+	 * @private
+	 */
+	inject: [
+		'eventManager'
+	],
 
-    /**
-     * @inheritdoc
-     */
-    config: {
-        /**
-         * Event manager
-         * @private
-         */
-        eventManager: null
-    },
+	/**
+	 * @inheritdoc
+	 */
+	config: {
+		/**
+		 * Event manager
+		 * @private
+		 */
+		eventManager: null
+	},
 
-    /**
-     * @inheritdoc
-     */
-    init: function (field) {
-        field.__fieldTriggerScope = {};
+	/**
+	 * @inheritdoc
+	 */
+	init: function (field) {
+		field.__fieldTriggerScope = {};
 
-        field.setUI(this.triggerUI);
+		field.setUI(this.triggerUI);
 
-        field.on({
-            scope: this,
-            focus: this.onFieldFocus,
-            afterrender: this.onFieldAfterRender,
-            specialkey: this.onSpecialKey
-        });
-    },
+		field.on({
+			scope: this,
+			focus: this.onFieldFocus,
+			afterrender: this.onFieldAfterRender,
+			specialkey: this.onSpecialKey,
+			hasfieldtargetaction: this.hasFieldTargetAction
+		});
+	},
 
-    /**
-     * Special key handler
-     */
-    onSpecialKey: function (field, e) {
-        switch (e.getKey()) {
-            case e.ENTER:
-                this.onEnterKey(field);
-                break;
-            case e.ESC:
-                this.onEscKey(field);
-                break;
-        }
+	/**
+	 * Special key handler
+	 */
+	onSpecialKey: function (field, e) {
+		var keyHandlerResult;
 
-        if (Ext.Array.contains([e.ENTER, e.ESC], e.getKey())) {
-            this.processGlobalDown(field);
-        }
-    },
+		switch (e.getKey()) {
+			case e.ENTER:
+				keyHandlerResult = this.onEnterKey(field);
+				break;
+			case e.ESC:
+				keyHandlerResult = this.onEscKey(field);
+				break;
+		}
 
-    /**
-     * Special Enter key handler
-     *
-     * @protected
-     * @param {Ext.form.field.Field} field Field
-     */
-    onEnterKey: Ext.emptyFn,
+		// Prevent default plugin behaviour
+		if (keyHandlerResult === false) {
+			return;
+		}
 
-    /**
-     * Special Esc key handler
-     *
-     * @@protected
-     * @param {Ext.form.field.Field} field Field
-     */
-    onEscKey: Ext.emptyFn,
+		if (Ext.Array.contains([e.ENTER, e.ESC], e.getKey())) {
+			this.processGlobalDown(field);
+		}
+	},
 
-    /**
-     * Field after render handler
-     *
-     * @protected
-     * @param {Ext.form.field.Field} field Field
-     */
-    onFieldAfterRender: function (field) {
-        var el,
-            me = this,
-            inputEl = field.inputEl,
-            actionsTotalWidth = 0,
-            config = field.fieldTriggerConfig,
-            actions = config && config.actions ? config.actions : Ext.clone(this.actions),
-            fieldTriggerScope = field.__fieldTriggerScope;
+	/**
+	 * Special Enter key handler
+	 *
+	 * @protected
+	 * @param {Ext.form.field.Field} field Field
+	 */
+	onEnterKey: Ext.emptyFn,
 
-        fieldTriggerScope.actionsDomElements = {};
+	/**
+	 * Special Esc key handler
+	 *
+	 * @@protected
+	 * @param {Ext.form.field.Field} field Field
+	 */
+	onEscKey: Ext.emptyFn,
 
-        Ext.Array.each(actions.reverse(), function (action, index) {
-            var customActionConfig;
+	/**
+	 * Field after render handler
+	 *
+	 * @protected
+	 * @param {Ext.form.field.Field} field Field
+	 */
+	onFieldAfterRender: function (field) {
+		var el,
+			me = this,
+			inputEl = field.inputEl,
+			actionsTotalWidth = 0,
+			config = field.fieldTriggerConfig,
+			actions = config && config.actions ? config.actions : Ext.clone(this.actions),
+			fieldTriggerScope = field.__fieldTriggerScope;
 
-            if (!Ext.isString(action)) {
-                /**
-                 * Support of custom widget config such as combobox, etc
-                 */
-                customActionConfig = Ext.clone(action);
-                action = 'custom- ' + index;
-            }
+		fieldTriggerScope.actionsDomElements = {};
 
-            el = fieldTriggerScope.actionsDomElements[action] = Ext.DomHelper.insertAfter(inputEl, {
-                tag: 'div',
-                cls: Ext.String.format(
-                    '{1} {2}placeholder {2}{0}',
-                    action,
-                    customActionConfig ? '' : me.fieldTriggerPrefixCls + 'action',
-                    me.fieldTriggerPrefixCls
-                ),
-                action: action
-            });
+		Ext.Array.each(actions.reverse(), function (action, index) {
+			var customActionConfig;
 
-            if (customActionConfig) {
-                customActionConfig.renderTo = el;
-                Ext.widget(customActionConfig);
-            }
+			if (!Ext.isString(action)) {
+				/**
+				 * Support of custom widget config such as combobox, etc
+				 */
+				customActionConfig = Ext.clone(action);
+				action = 'custom- ' + index;
+			}
 
-            el = Ext.fly(el);
-            actionsTotalWidth += el.getWidth() + el.getMargin('lr');
-        });
+			el = fieldTriggerScope.actionsDomElements[action] = Ext.DomHelper.insertAfter(inputEl, {
+				tag: 'div',
+				cls: Ext.String.format(
+					'{1} {2}placeholder {2}{0}',
+					action,
+					customActionConfig ? '' : me.fieldTriggerPrefixCls + 'action',
+					me.fieldTriggerPrefixCls
+				),
+				action: action
+			});
 
-        // TODO Support layout
-        inputEl.setWidth(inputEl.getWidth() - actionsTotalWidth);
-    },
+			if (customActionConfig) {
+				customActionConfig.renderTo = el;
+				Ext.widget(customActionConfig);
+			}
 
-    /**
-     * Field focus handler
-     *
-     * @protected
-     * @param {Ext.form.field.Field} field Field
-     */
-    onFieldFocus: function (field) {
-        this.bindGlobalDown(field);
-    },
+			el = Ext.fly(el);
+			actionsTotalWidth += el.getWidth() + el.getMargin('lr');
+		});
+	},
 
-    /**
-     * Get trigger action dom element by name
-     *
-     * @protected
-     * @param {Ext.form.field.Field} field Field
-     * @param {String} actionName Action name
-     * @returns {HTMLElement}
-     */
-    getActionDomElement: function (field, actionName) {
-        return field.__fieldTriggerScope.actionsDomElements[actionName];
-    },
+	/**
+	 * Has field target action
+	 *
+	 * @param field Field
+	 * @param target Target dom element
+	 */
+	hasFieldTargetAction: function (field, target) {
+		var hasFieldTargetAction = false;
+		Ext.Object.each(this.getActionDomElements(field), function (key, actionDomElement) {
+			target === actionDomElement && (hasFieldTargetAction |= true);
+		});
+		return hasFieldTargetAction;
+	},
 
-    /**
-     * Global down handler
-     *
-     * @param {Object} event Event
-     * @param {HTMLElement} target target
-     * @param {Ext.form.field.Field} field Field
-     */
-    onGlobalDown: function (event, target, field) {
-        this.processGlobalDown(field, target);
-    },
+	/**
+	 * Field focus handler
+	 *
+	 * @protected
+	 * @param {Ext.form.field.Field} field Field
+	 */
+	onFieldFocus: function (field) {
+		this.bindGlobalDown(field);
+	},
 
-    /**
-     * Process global down
-     *
-     * @protected
-     * @param {HTMLElement} target target
-     * @param {Ext.form.field.Field} field Field
-     */
-    processGlobalDown: function (field, target) {
-        Ext.log({msg: '[FIELD_TRIGGER]: process global down'}, ' >> field name: ' + field.getName());
+	/**
+	 * Get trigger action dom element by name
+	 *
+	 * @protected
+	 * @param {Ext.form.field.Field} field Field
+	 * @param {String} actionName Action name
+	 * @returns {HTMLElement}
+	 */
+	getActionDomElement: function (field, actionName) {
+		return this.getActionDomElements(field)[actionName];
+	},
 
-        /**
-         * Unbind global down listener when deactivated
-         */
-        this.unbindGlobalDown(field);
-    },
+	/**
+	 * Get trigger actions dom elements
+	 *
+	 * @protected
+	 * @param {Ext.form.field.Field} field Field
+	 * @returns {Array}
+	 */
+	getActionDomElements: function (field) {
+		return field.__fieldTriggerScope.actionsDomElements;
+	},
 
-    /**
-     * Bind global down
-     *
-     * @param field Field
-     */
-    bindGlobalDown: function (field) {
-        field.mon(this.getEventManager(), 'globaldown', this.onGlobalDown, this, field);
-    },
+	/**
+	 * Global down handler
+	 *
+	 * @param {Object} event Event
+	 * @param {HTMLElement} target target
+	 * @param {Ext.form.field.Field} field Field
+	 */
+	onGlobalDown: function (event, target, field) {
+		this.processGlobalDown(field, target);
+	},
 
-    /**
-     * Unbind global down
-     *
-     * @param field Field
-     */
-    unbindGlobalDown: function (field) {
-        field.mun(this.getEventManager(), 'globaldown', this.onGlobalDown, this);
-    }
+	/**
+	 * Process global down
+	 *
+	 * @protected
+	 * @param {HTMLElement} target target
+	 * @param {Ext.form.field.Field} field Field
+	 */
+	processGlobalDown: function (field, target) {
+		// Unbind global down listener when the field deactivated
+		this.unbindGlobalDown(field);
+	},
+
+	/**
+	 * Bind global down listener for the current active field
+	 *
+	 * @param field Field
+	 */
+	bindGlobalDown: function (field) {
+		Ext.log({msg: '[FIELD_TRIGGER]: bind global down'}, ' >> field name: ' + field.getName());
+
+		field.mon(this.getEventManager(), 'globaldown', this.onGlobalDown, this, field);
+	},
+
+	/**
+	 * Unind global down listener for the current active field
+	 *
+	 * @param field Field
+	 */
+	unbindGlobalDown: function (field) {
+		Ext.log({msg: '[FIELD_TRIGGER]: unbind global down'}, ' >> field name: ' + field.getName());
+
+		field.mun(this.getEventManager(), 'globaldown', this.onGlobalDown, this);
+	}
 });

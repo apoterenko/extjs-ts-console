6ccb31a8ec1af450c8d23f8551e4ae4c03f35ce0
apoterenko <a.poterenko@gmail.com>
2015-11-30 17:18:41 +0300

    * bugfix at Model

--- app/overrides/Model.js
+++ app/overrides/Model.js
@@ -37,6 +37,8 @@ Ext.define('ManagementConsole.overrides.Model', {
 
 		if (config.callback) {
 			config.callback = Ext.Function.createSequence(config.callback, callback, config.scope || me);
+		} else {
+			config.callback = callback;
 		}
 		return this.callParent([config]);
 	}

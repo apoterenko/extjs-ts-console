7cd107d4837d8d1811ef6d4ea5b36df46fcdfbc2
apoterenko <a.poterenko@gmail.com>
2015-11-30 17:56:04 +0300

    *added bootstrapT.ts

---
+++ app/bootstrapT.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings_fix/tsd.d.ts"/>
+
+/**
+ * This file loads all the amd dependencies.
+ */
+export import editorManagerT = require("./service/editor/EditorManagerT");
+export import GroupT = require("./model/GroupT");
+export import MainViewT = require("./view/main/MainViewT");
+export import MainControllerT = require("./view/main/MainControllerT");
+export import NavigatorViewT = require("./view/navigation/NavigatorViewT");
+export import OperationalViewT = require("./view/operation/OperationalViewT");

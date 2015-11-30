7c893ecdf04754f7f66ec2dda0cb4b7ac1c38996
apoterenko <a.poterenko@gmail.com>
2015-11-30 16:35:34 +0300

    *added Model overrides

---
+++ app/overrides/Model.js
@@ -0,0 +1,43 @@
+/**
+ * @class ManagementConsole.overrides.Model
+ */
+Ext.define('ManagementConsole.overrides.Model', {
+	override: 'Ext.data.Model',
+
+	mixins: ['Ext.mixin.Observable'],
+
+	// @inheritdoc
+	config: {
+
+		/**
+		 * If the record has an existing ID, but the record not loaded yet
+		 * @private
+		 */
+		newest: null
+	},
+
+	// @override
+	constructor: function () {
+		this.mixins.observable.constructor.apply(this, arguments);
+		this.callParent(arguments);
+	},
+
+	/**
+	 * @override
+	 * @param {Object} config Config
+	 */
+	load: function (config) {
+		var me = this,
+			callback = function () {
+				me.setNewest(false);
+				me.fireEvent('load', me);
+			};
+
+		config = config || {};
+
+		if (config.callback) {
+			config.callback = Ext.Function.createSequence(config.callback, callback, config.scope || me);
+		}
+		return this.callParent([config]);
+	}
+});

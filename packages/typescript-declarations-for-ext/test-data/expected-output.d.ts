20b366d22cce7a9741b26c9e40f2a407927c1113
apoterenko <a.poterenko@gmail.com>
2015-12-02 20:31:18 +0300

    *added typescript-declarations-for-ext

---
+++ packages/typescript-declarations-for-ext/test-data/expected-output.d.ts
@@ -0,0 +1,29 @@
+// Ext type declarations generated on Fri Nov 21 2014 22:22:36 GMT+0000 (GMT)
+// For more information, see: https://github.com/Dretch/typescript-declarations-for-ext
+declare module Ext {
+    export class TestClass {
+        stringEnumConfig: string;
+        numberProperty: number;
+        unionTypeProperty: number|number[];
+        objectProperty: any;
+        'property-with-dashes': any;
+        undefinedProperty: void;
+        propertyWithNoneExistantClass: any;
+        objectArrayProperty: any[];
+        static optionalStaticArrayProperty?: string[];
+        constructor(n: number, ...items: string[]);
+        setNumberProperty(newNumber: number):Ext.TestClass;
+        methodFromMixinNotOverridden():void;
+        methodFromMixinOverridden():void;
+        methodWithCallback(f: (x: string) => boolean):void;
+        staticMethod():void;
+    }
+    export class TestEnum {
+    }
+    export class TestSubClass extends Ext.TestSuperClass {
+        privateMethodInSuperClass():void;
+        eventInSuperClass():void;
+    }
+    export class TestSuperClass {
+    }
+}
